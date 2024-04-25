73b52d4dc768c30494881b0ad7bd565237e10acf
Mohamed abo el maaty zaky <98196426+MohamedAboElM3aTy@users.noreply.github.com>
2024-04-25 09:35:59 +0200

    add: app helpers

---
+++ lib/utils/app_styles.dart
@@ -0,0 +1,173 @@
+// import 'package:flutter/material.dart';
+
+// @immutable
+// class AppStyles {
+//   const AppStyles._();
+
+//   static TextStyle styleRegular16(context) {
+//     return TextStyle(
+//       color: const Color(0xFF064060),
+//       fontSize: getResponsiveFontSize(context, fontSize: 16),
+//       fontFamily: 'Montserrat',
+//       fontWeight: FontWeight.w400,
+//     );
+//   }
+
+//   static TextStyle styleBold16(BuildContext context) {
+//     return TextStyle(
+//       color: const Color(0xFF4EB7F2),
+//       fontSize: getResponsiveFontSize(context, fontSize: 16),
+//       fontFamily: 'Montserrat',
+//       fontWeight: FontWeight.w700,
+//     );
+//   }
+
+//   static TextStyle styleMedium16(BuildContext context) {
+//     return TextStyle(
+//       color: const Color(0xFF064061),
+//       fontSize: getResponsiveFontSize(context, fontSize: 16),
+//       fontFamily: 'Montserrat',
+//       fontWeight: FontWeight.w500,
+//     );
+//   }
+
+//   static TextStyle styleMedium20(BuildContext context) {
+//     return TextStyle(
+//       color: const Color(0xFFFFFFFF),
+//       fontSize: getResponsiveFontSize(context, fontSize: 20),
+//       fontFamily: 'Montserrat',
+//       fontWeight: FontWeight.w500,
+//     );
+//   }
+
+//   static TextStyle styleSemiBold16(BuildContext context) {
+//     return TextStyle(
+//       color: const Color(0xFF064061),
+//       fontSize: getResponsiveFontSize(context, fontSize: 16),
+//       fontFamily: 'Montserrat',
+//       fontWeight: FontWeight.w600,
+//     );
+//   }
+
+//   static TextStyle styleSemiBold20(BuildContext context) {
+//     return TextStyle(
+//       color: const Color(0xFF064061),
+//       fontSize: getResponsiveFontSize(context, fontSize: 20),
+//       fontFamily: 'Montserrat',
+//       fontWeight: FontWeight.w600,
+//     );
+//   }
+
+//   static TextStyle styleRegular12(BuildContext context) {
+//     return TextStyle(
+//       color: const Color(0xFFAAAAAA),
+//       fontSize: getResponsiveFontSize(context, fontSize: 12),
+//       fontFamily: 'Montserrat',
+//       fontWeight: FontWeight.w400,
+//     );
+//   }
+
+//   static TextStyle styleSemiBold24(BuildContext context) {
+//     return TextStyle(
+//       color: const Color(0xFF4EB7F2),
+//       fontSize: getResponsiveFontSize(context, fontSize: 24),
+//       fontFamily: 'Montserrat',
+//       fontWeight: FontWeight.w600,
+//     );
+//   }
+
+//   static TextStyle styleRegular14(BuildContext context) {
+//     return TextStyle(
+//       color: const Color(0xFFAAAAAA),
+//       fontSize: getResponsiveFontSize(context, fontSize: 14),
+//       fontFamily: 'Montserrat',
+//       fontWeight: FontWeight.w400,
+//     );
+//   }
+
+//   static TextStyle styleSemiBold18(BuildContext context) {
+//     return TextStyle(
+//       color: const Color(0xFFFFFFFF),
+//       fontSize: getResponsiveFontSize(context, fontSize: 18),
+//       fontFamily: 'Montserrat',
+//       fontWeight: FontWeight.w600,
+//     );
+//   }
+// }
+
+import 'package:flutter/material.dart';
+
+@immutable
+class AppStyles {
+  const AppStyles._();
+
+  static const TextStyle styleRegular16 = TextStyle(
+    color: Color(0xFF064060),
+    fontSize: 16,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle styleBold16 = TextStyle(
+    color: Color(0xFF4EB7F2),
+    fontSize: 16,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w700,
+  );
+
+  static const TextStyle styleMedium16 = TextStyle(
+    color: Color(0xFF064061),
+    fontSize: 16,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle styleMedium20 = TextStyle(
+    color: Color(0xFFFFFFFF),
+    fontSize: 20,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle styleSemiBold16 = TextStyle(
+    color: Color(0xFF064061),
+    fontSize: 16,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle styleSemiBold20 = TextStyle(
+    color: Color(0xFF064061),
+    fontSize: 20,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle styleRegular12 = TextStyle(
+    color: Color(0xFFAAAAAA),
+    fontSize: 12,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle styleSemiBold24 = TextStyle(
+    color: Color(0xFF4EB7F2),
+    fontSize: 24,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle styleRegular14 = TextStyle(
+    color: Color(0xFFAAAAAA),
+    fontSize: 14,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle styleSemiBold18 = TextStyle(
+    color: Color(0xFFFFFFFF),
+    fontSize: 18,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+  );
+}
